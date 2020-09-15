c211bbd6e4d6869d1158a7c76fc8790a59d2a5f6
Parminder Gill <pgill18@gmail.com>
2020-09-15 14:05:15 +0100

    added single answer edit. also enhanced visuals for building panels.

--- quizes.js
+++ quizes.js
@@ -1,5 +1,12 @@
 let MAX_CHOICES = 4;
+let MAX_QUIZES = 6;
 let quizes = [];
+quizes.push(content_quiz1());
+quizes.push(content_quiz2());
+quizes.push(content_quiz3());
+quizes.push(content_quiz1());
+quizes.push(content_quiz2());
+quizes.push(content_quiz3());
 quizes.push(content_quiz1());
 quizes.push(content_quiz2());
 quizes.push(content_quiz3());
@@ -72,7 +79,7 @@ function content_quiz3() {
     ['quingombó', 'Okra'],
     ['maíz',    'Corn'],
     ['lenteja', 'Lentil'],
-    ['patata', 'Spinach'],
+    ['espinacas', 'Spinach'],
     ['tomate', 'Tomato'],
     ['jengibre', 'Ginger'],
     ['ajo', 'Garlic'],
